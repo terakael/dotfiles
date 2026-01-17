59e6954149533a5e834410ce90c06c8cf86a4616
Knezevic, Daniel | Dan | ECFD <daniel.knezevic@rakuten.com>
2026-01-17 19:00:03 +0900

    [win11] helm highlighting

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -32,3 +32,43 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave', 'CursorHo
     end
   end,
 })
+
+-- Detect Helm template files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  desc = 'Set filetype for Helm template files',
+  group = vim.api.nvim_create_augroup('helm-filetype', { clear = true }),
+  pattern = { '*/templates/*.yaml', '*/templates/*.tpl', 'helmfile.yaml' },
+  callback = function()
+    vim.bo.filetype = 'helm'
+  end,
+})
+
+-- Enhanced Helm syntax highlighting
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Apply enhanced highlighting for Helm templates',
+  group = vim.api.nvim_create_augroup('helm-highlights', { clear = true }),
+  pattern = 'helm',
+  callback = function()
+    local colors = require('forest.colors')
+    
+    -- Highlight template brackets distinctly (like vim-helm's PreProc)
+    vim.api.nvim_set_hl(0, '@punctuation.bracket.helm', { fg = colors.red, bold = true })
+    vim.api.nvim_set_hl(0, '@punctuation.bracket.gotmpl', { fg = colors.red, bold = true })
+    
+    -- Helm-specific constants (.Values, .Release, .Chart)
+    vim.api.nvim_set_hl(0, '@constant.builtin.helm', { fg = colors.bright_magenta, bold = true })
+    vim.api.nvim_set_hl(0, '@constant.builtin.gotmpl', { fg = colors.bright_magenta, bold = true })
+    
+    -- Template variables and nested properties
+    vim.api.nvim_set_hl(0, '@variable.member.helm', { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, '@variable.member.gotmpl', { fg = colors.yellow })
+    
+    -- YAML properties (keys)
+    vim.api.nvim_set_hl(0, '@property', { fg = colors.blue, bold = true })
+    vim.api.nvim_set_hl(0, '@string.yaml', { fg = colors.cyan })
+    
+    -- Sprig/template functions
+    vim.api.nvim_set_hl(0, '@function.builtin.helm', { fg = colors.green, bold = true })
+    vim.api.nvim_set_hl(0, '@function.builtin.gotmpl', { fg = colors.green, bold = true })
+  end,
+})
