b80ab1a75fd048e3a8cd46da61aee43b53ed8e72
Dan <terakael@gmail.com>
2026-08-20 08:31:51 +0900

    [win11] more statusbar qol

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -46,37 +46,41 @@ return { -- Collection of various small independent plugins/modules
     -- Customize active content to place harpoon/filename on the right
     ---@diagnostic disable-next-line: duplicate-set-field
     statusline.active = function()
-      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-      local git           = statusline.section_git({ trunc_width = 40 })
-      local diff          = statusline.section_diff({ trunc_width = 75 })
-      local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
-      local lsp           = statusline.section_lsp({ trunc_width = 75 })
-      local filename      = statusline.section_filename({ trunc_width = 140 })
-      local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
-      local location      = statusline.section_location({ trunc_width = 75 })
-      local search        = statusline.section_searchcount({ trunc_width = 75 })
+      local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+      local git = statusline.section_git { trunc_width = 40 }
+      local diff = statusline.section_diff { trunc_width = 75 }
+      local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+      local lsp = statusline.section_lsp { trunc_width = 75 }
+      local filename = statusline.section_filename { trunc_width = 140 }
+      local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+      local location = statusline.section_location { trunc_width = 75 }
+      local search = statusline.section_searchcount { trunc_width = 75 }
 
-      return statusline.combine_groups({
-        { hl = mode_hl,                  strings = { mode } },
-        { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+      return statusline.combine_groups {
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
         '%<', -- Mark general truncate point
         '%=', -- Right-align everything after this point
         { hl = 'MiniStatuslineFilename', strings = { filename } },
         { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-        { hl = mode_hl,                  strings = { search, location } },
-      })
+        { hl = mode_hl, strings = { search, location } },
+      }
     end
+
+    -- Link CustomHarpoonActive to mode_hl (same as search/location highlight)
+    -- or we can link it directly to a theme group like 'Visual' or 'PmenuSel'
+    vim.api.nvim_set_hl(0, 'CustomHarpoonActive', { link = 'MiniStatuslineFileinfo' })
 
     -- Override filename section to show Harpoon list status
     ---@diagnostic disable-next-line: duplicate-set-field
     statusline.section_filename = function()
       local success, harpoon = pcall(require, 'harpoon')
       if not success then
-        return vim.fn.expand('%:t')
+        return vim.fn.expand '%:t'
       end
 
       local list = harpoon:list()
-      local current_file = vim.fn.expand('%:f')
+      local current_file = vim.fn.expand '%:f'
       local current_abs = vim.fn.fnamemodify(current_file, ':p')
 
       local items = {}
@@ -86,18 +90,28 @@ return { -- Collection of various small independent plugins/modules
         local filename = vim.fn.fnamemodify(item.value, ':t')
         local item_abs = vim.fn.fnamemodify(item.value, ':p')
 
+        local modified_mark = ''
+        local bufnr = vim.fn.bufnr(item.value)
+        if bufnr ~= -1 and vim.bo[bufnr].modified then
+          modified_mark = '*'
+        end
+
         if item_abs == current_abs then
           found_in_list = true
-          table.insert(items, string.format('%%#MiniStatuslineFilename#[%d: %s]', index, filename))
+          if vim.bo.modified then
+            modified_mark = '*'
+          end
+          table.insert(items, string.format('%%#CustomHarpoonActive#%d %s%s%%#MiniStatuslineFilename#', index, filename, modified_mark))
         else
-          table.insert(items, string.format('%d: %s', index, filename))
+          table.insert(items, string.format('%d %s%s', index, filename, modified_mark))
         end
       end
 
       -- If current file is not in harpoon list, append it without a number
       if not found_in_list and current_file ~= '' then
         local current_filename = vim.fn.fnamemodify(current_file, ':t')
-        table.insert(items, string.format('%%#MiniStatuslineFilename#[%s]', current_filename))
+        local modified_mark = vim.bo.modified and '*' or ''
+        table.insert(items, string.format('%%#CustomHarpoonActive#%s%s%%#MiniStatuslineFilename#', current_filename, modified_mark))
       end
 
       if #items == 0 then
